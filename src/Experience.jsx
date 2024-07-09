1008ff71760a8ec24bfed4f85190fb7d17224a82
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-09 15:53:13 +0800

    feat: CreateExperience component to hold details about previous-work experience

---
+++ src/Experience.jsx
@@ -0,0 +1,12 @@
+import Input from "./Input";
+
+export default function Experience() {
+    return (
+        <div className="experience">
+            <Input label="Work Title" placeholder="Cloud Engineer" className="work-title"></Input>
+            <Input label="Company" placeholder="Netflix" className="company"></Input>
+            <Input label="Year" placeholder="2024" className="working-years"></Input>
+            <Input label="Description" placeholder="Add Description" className="job-description"></Input>
+        </div>
+    )
+}
