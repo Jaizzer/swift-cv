24549df075f1da2bdfe8c476dbe5a2cef9cbbd88
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-10 14:37:20 +0800

    feat: Make working-years input label a range

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -5,7 +5,7 @@ export default function Experience() {
         <div className="experience">
             <Input label="Work Title" placeholder="Cloud Engineer" className="work-title"></Input>
             <Input label="Company" placeholder="Netflix" className="company"></Input>
-            <Input label="Year" placeholder="2024" className="working-years"></Input>
+            <Input label="Year" placeholder="2023-2024" className="working-years"></Input>
             <Input label="Description" placeholder="Add Description" className="job-description"></Input>
         </div>
     )
