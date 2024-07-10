96a6df16332d1cbf3fab2c983e101daed4890263
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-10 17:06:41 +0800

    feat: Test the layout of Experience, Contact, and Education components

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Experience from './Experience'
+import Contact from './Contact'
+import Education from './Education'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <Experience></Experience>
+    <Contact></Contact>
+    <Education></Education>
   </React.StrictMode>,
 )
