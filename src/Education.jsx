5f6bdc90a6d9726b93f1c28e09029cf047e7e19e
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-10 16:22:40 +0800

    feat: Add Education component to hold previous educational experience

---
+++ src/Education.jsx
@@ -0,0 +1,11 @@
+import Input from './Input';
+
+export default function Education() {
+    return (
+        <div className="education">
+            <Input label="Course" placeholder="BS Computer Science" className="course"></Input>
+            <Input label="Institution" placeholder="Stanford University" className="institution"></Input>
+            <Input label="Year" placeholder="2023-2024" className="education-years"></Input>
+        </div>
+    );
+}
