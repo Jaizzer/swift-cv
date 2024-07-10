a867593cfaa251fac23a0fac3964c9fa4d6544c3
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-11 07:52:56 +0800

    feat: Create initial Area component feature which returns the parent 'cv-area' that will hold children component inside

---
+++ src/Area.jsx
@@ -0,0 +1,8 @@
+
+export default function Area({heading, children}) {
+    return (
+        <div className="cv-area">
+            <div className="heading">{heading}</div>
+        </div>
+    )
+}
