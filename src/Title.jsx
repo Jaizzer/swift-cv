5da12d69555ef27c15875157ca14de286e7aa987
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-11 07:25:38 +0800

    feat: Create title component to hold person's work title

---
+++ src/Title.jsx
@@ -0,0 +1,7 @@
+import Input from "./Input";
+
+export default function Title() {
+    return(
+        <Input placeholder="Software Engineer" label="Work Title" className="work-title"></Input>
+    )
+}
