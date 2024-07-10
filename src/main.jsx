d20c18b388de9d7f96d3575ad2e95ec9f7262151
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-11 07:33:04 +0800

    feat: Try out newly added components

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,16 @@ import './index.css'
 import Experience from './Experience'
 import Contact from './Contact'
 import Education from './Education'
+import Image from './Image'
+import Name from './Name'
+import Title from './Title'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div>Website not yet completed!</div>
+    <Name></Name>
+    <Title></Title>
+    <Image></Image>
     <Experience></Experience>
     <Contact></Contact>
     <Education></Education>
