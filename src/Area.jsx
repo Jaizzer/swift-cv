db3a5969fbd5cf10d45b1067d111a0dbd91b95eb
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-11 09:22:42 +0800

    style: Format code

--- src/Area.jsx
+++ src/Area.jsx
@@ -5,29 +5,30 @@ export default function Area({ heading, children }) {
     const [areaChildren, setAreaChildren] = useState(children);
 
     function handleClick(childToDelete) {
-        setAreaChildren(areaChildren.filter((areaChild) => {
-            return areaChild !== childToDelete;
-        }))
+        setAreaChildren(
+            areaChildren.filter((areaChild) => {
+                return areaChild !== childToDelete;
+            })
+        );
     }
 
     return (
         <div className="cv-area">
             <div className="heading">{heading}</div>
-            {
-                areaChildren.map((areaChild, index) => (
-                    // eslint-disable-next-line react/jsx-key
-                    <div className="cv-area-item" key={index}>
-                        {areaChild}
-                        <button className="delete" onClick={
-                            () => {
-                                handleClick(areaChild)
-                            }
-                        }>
-                            Delete
-                        </button>
-                    </div>
-                ))
-            }
+            {areaChildren.map((areaChild, index) => (
+                // eslint-disable-next-line react/jsx-key
+                <div className="cv-area-item" key={index}>
+                    {areaChild}
+                    <button
+                        className="delete"
+                        onClick={() => {
+                            handleClick(areaChild);
+                        }}
+                    >
+                        Delete
+                    </button>
+                </div>
+            ))}
         </div>
     );
 }
