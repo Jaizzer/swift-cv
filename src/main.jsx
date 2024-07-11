bc6d4cf0a4784d49ff3da75d1b1602bf03f3a2cd
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-11 15:14:25 +0800

    feat: Try the Area component

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,22 @@ import Education from './Education'
 import Image from './Image'
 import Name from './Name'
 import Title from './Title'
+import Area from './Area'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div>Website not yet completed!</div>
+    <Image></Image>
     <Name></Name>
     <Title></Title>
-    <Image></Image>
-    <Experience></Experience>
-    <Contact></Contact>
-    <Education></Education>
+    <Area heading="Education">
+        <Education></Education>
+    </Area>
+    <Area heading="Work Experience">
+        <Experience></Experience>
+    </Area>
+    <Area heading="Contact">
+        <Contact></Contact>
+    </Area>
   </React.StrictMode>,
 )
