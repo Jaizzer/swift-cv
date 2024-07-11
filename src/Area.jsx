a5080ce0b9ad0736217e8665a4b8ee281a4c468f
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-11 14:52:39 +0800

    fix: Use keys when deleting children component to avoid wrong component deletions

--- src/Area.jsx
+++ src/Area.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 
 /* eslint-disable react/prop-types */
 export default function Area({ heading, children }) {
-    const [areaChildren, setAreaChildren] = useState(children);
+    let [areaChildren, setAreaChildren] = useState(
+        // Add unique key to children component
+        children.map((areaChild) => {
+            let uniqueId = Math.floor(Math.random() * 10000000000);
+            return <Fragment key={uniqueId}>{areaChild}</Fragment>;
+        })
+    );
 
-    function handleClick(childToDelete) {
-        setAreaChildren(
-            areaChildren.filter((areaChild) => {
-                return areaChild !== childToDelete;
-            })
-        );
+    function handleClick(keyOfAreaChildToDelete) {
+        // Remove the area child with the matches the 'key of area child to delete'
+        const newArray = areaChildren.filter((areaChild) => {
+            return areaChild.key !== keyOfAreaChildToDelete;
+        });
+
+        // Update state
+        setAreaChildren(newArray);
     }
 
     return (
         <div className="cv-area">
             <div className="heading">{heading}</div>
-            {areaChildren.map((areaChild, index) => (
-                // eslint-disable-next-line react/jsx-key
-                <div className="cv-area-item" key={index}>
-                    {areaChild}
-                    <button
-                        className="delete"
-                        onClick={() => {
-                            handleClick(areaChild);
-                        }}
-                    >
-                        Delete
-                    </button>
-                </div>
-            ))}
+
+            {/* {render children components with corresponding delete key} */}
+            {areaChildren.map((areaChild) => {
+                return (
+                    <div className="cv-area-item" key={areaChild.key}>
+                        {areaChild}
+                        <button
+                            className="delete"
+                            onClick={() => {
+                                handleClick(areaChild.key);
+                            }}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                );
+            })}
         </div>
     );
 }
