a8e052e0a08023ab2e67b10bb6ac9ebbbe71ca6e
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-11 08:10:49 +0800

    style: Format the code

--- src/Area.jsx
+++ src/Area.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable react/prop-types */
-export default function Area({heading, children}) {
-    const childrenWithKey = children.map(child => <div className="cv-area-item" key={Math.random()}>{child}</div>)
+export default function Area({ heading, children }) {
+    const childrenWithKey = children.map((child) => (
+        <div className="cv-area-item" key={Math.random()}>
+            {child}
+        </div>
+    ));
 
     return (
         <div className="cv-area">
             <div className="heading">{heading}</div>
             {childrenWithKey}
         </div>
-    )
+    );
 }
