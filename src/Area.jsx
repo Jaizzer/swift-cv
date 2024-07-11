0ed47aabc4c5bee0aa31f5ad3b982322185725e6
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-11 08:10:01 +0800

    feat: Add unique keys to children passed inside Area component to make room for array modification features later

--- src/Area.jsx
+++ src/Area.jsx
@@ -1,8 +1,11 @@
-
+/* eslint-disable react/prop-types */
 export default function Area({heading, children}) {
+    const childrenWithKey = children.map(child => <div className="cv-area-item" key={Math.random()}>{child}</div>)
+
     return (
         <div className="cv-area">
             <div className="heading">{heading}</div>
+            {childrenWithKey}
         </div>
     )
 }
