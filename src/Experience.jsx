783514412454766e395c5df0d8c8cedf0bded8c9
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-13 12:19:50 +0800

    feat: Add new longer description for better visualization in the layout

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import Input from "./Input";
+import Input from './Input';
 
 export default function Experience() {
     return (
@@ -6,7 +6,16 @@ export default function Experience() {
             <Input label="Work Title" placeholder="Cloud Engineer" className="work-title"></Input>
             <Input label="Company" placeholder="Netflix" className="company"></Input>
             <Input label="Year" placeholder="2023-2024" className="working-years"></Input>
-            <Input label="Description" placeholder="Add Description" className="job-description"></Input>
+            <Input
+                label="Description"
+                placeholder="
+                    As a Software Engineer, I developed and maintained scalable web applications, 
+                    enhancing user experience and system performance. I collaborated with cross-functional
+                    teams to design and implement innovative solutions, utilizing Agile methodologies to 
+                    ensure timely and efficient project delivery. I also conducted thorough code reviews and 
+                    implemented best practices to maintain high standards of code quality."
+                className="job-description"
+            ></Input>
         </div>
-    )
+    );
 }
