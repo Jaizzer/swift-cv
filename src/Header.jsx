399b66eb879d899c68e7834433272820a138e7ea
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-13 11:03:51 +0800

    feat: Update class name to header

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ import Input from "./Input";
 
 export default function Header() {
     return (
-        <div className="name">
+        <div className="header">
             <Input placeholder="John" label="First name" className="first-name"></Input>
             <Input placeholder="Smith" label="Last name" className="last-name"></Input>
             <Input placeholder="Software Engineer" label="Work Title" className="work-title"></Input>
