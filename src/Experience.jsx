9f357d57611e8a2fdb993f20fa62be2db45c41d7
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-14 16:21:33 +0800

    feat: Update placeholder text

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,13 +3,13 @@ import Input from './Input';
 export default function Experience() {
     return (
         <div className="experience">
-            <Input label="Work Title" placeholder="Cloud Engineer" className="work-title"></Input>
+            <Input label="Work Title" placeholder="Full-Stack Developer" className="work-title"></Input>
             <Input label="Company" placeholder="Netflix" className="company"></Input>
             <Input label="Year" placeholder="2023-2024" className="working-years"></Input>
             <Input
                 label="Description"
                 placeholder="
-                    As a Software Engineer, I developed and maintained scalable web applications, 
+                    As a Full-Stack Developer, I developed and maintained scalable web applications, 
                     enhancing user experience and system performance. I collaborated with cross-functional
                     teams to design and implement innovative solutions, utilizing Agile methodologies to 
                     ensure timely and efficient project delivery. I also conducted thorough code reviews and 
